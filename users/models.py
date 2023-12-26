3f308561ceac1917455b11f13b1ee0ccec11ad86
Bazilahamid <151023422+Bazilahamid@users.noreply.github.com>
2023-12-26 20:12:17 +0530

    edited files

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
+class CustomUser(AbstractBaseUser):
+    
+    username = None
+    phone_number = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True, null=True)
+    user_bio = models.TextField(max_length=100, null=True)
+    user_profile_image = models.ImageField(upload_to="profile")
+    
+    USERNAME_FIELD =['phone_number']
+    REQUIRED_FIELDS =[]
