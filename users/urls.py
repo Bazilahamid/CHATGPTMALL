674b449ebe053453909aba4c32061dea8d506e64
Bazilahamid <151023422+Bazilahamid@users.noreply.github.com>
2023-12-26 15:55:52 +0530

    added files

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from users.views import HomePage, LoginView, RegisterView
+
+urlpatterns = [
+    
+    path('', HomePage, name='home'),
+    path('login/', LoginView, name='login'),
+    path('register/', RegisterView, name='register'),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def HomePage(request):
+    return render (request, 'homepage.html')
+
+def LoginView(request):
+    return render (request, 'login.html')
+
+def RegisterView(request):
+    return render (request, 'register.html')
+
+#def HomePage(request):
+ #def HomePage(request):
